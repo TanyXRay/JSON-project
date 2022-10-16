c7c7a94070162ee471c6a980ea80fb4ae063904b
Tatyana Chernyadieva <tany.taray@gmail.com>
2022-10-16 15:08:44 +0400

    fix readme,add xml file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+
+import static java.lang.Boolean.parseBoolean;
 
 /**
  * Решение задачи №1 по теме "Одномерные массивы".
@@ -11,17 +21,50 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         String[] products = {"Хлеб", "Молоко", "Яблоки"};
         int[] prices = {35, 87, 109};
 
         Scanner scanner = new Scanner(System.in);
         ClientLog clientLog = new ClientLog();
-        File csvFile = new File("log.csv");
-        File jsonFile = new File("basket.json");
         Basket basket = new Basket(products, prices);
 
-        if (jsonFile.exists()) {
+        File xmlFile = new File("xml_dir/shop.xml");
+        File csvFile = new File("log.csv");
+        File jsonFile = new File("basket.json");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlFile);
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+
+        boolean loadIsEnabled = false;
+        boolean saveIsEnabled = false;
+        boolean logIsEnabled = false;
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node tempNode = nodeList.item(i);
+            if ((tempNode.getNodeType() == Node.ELEMENT_NODE) && (tempNode.getNodeType() != Node.TEXT_NODE)) {
+                String nameParentNode = tempNode.getNodeName();
+                NodeList nodeList1 = tempNode.getChildNodes();
+                for (int j = 0; j < nodeList1.getLength(); j++) {
+                    Node twoNode = nodeList1.item(j);
+                    if ((twoNode.getNodeType() == Node.ELEMENT_NODE) && (twoNode.getNodeType() != Node.TEXT_NODE)) {
+                        String nameChildNode = twoNode.getNodeName();
+                        String text = twoNode.getTextContent();
+                        if (nameParentNode.equals("load") && nameChildNode.equals("enabled")) {
+                            loadIsEnabled = parseBoolean(text);
+                        } else if (nameParentNode.equals("save") && nameChildNode.equals("enabled")) {
+                            saveIsEnabled = parseBoolean(text);
+                        } else if (nameParentNode.equals("log") && nameChildNode.equals("enabled")) {
+                            logIsEnabled = parseBoolean(text);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (loadIsEnabled && jsonFile.exists()) {
             basket = Basket.loadFromJsonFile(jsonFile);
         }
         basket.printListAllProductsForBuy();
@@ -63,8 +106,12 @@ public class Main {
             basket.addToCart(productNumber, productCount);
             clientLog.log(productNumber + 1, productCount);
         }
-        clientLog.exportAsCSV(csvFile);
-        basket.saveJson(jsonFile);
+        if (logIsEnabled) {
+            clientLog.exportAsCSV(csvFile);
+        }
+        if (saveIsEnabled) {
+            basket.saveJson(jsonFile);
+        }
         basket.printCart();
     }
 }
